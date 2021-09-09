d84e0749ecd4ca716c70e24da6298e70d4969080
adrianpothuaud <adrian@trusk.com>
2021-09-09 10:43:04 +0200

    add phone to employee schema

--- trusk-x-api/src/domain/schemas/employee.schema.ts
+++ trusk-x-api/src/domain/schemas/employee.schema.ts
@@ -3,6 +3,7 @@ import {Schema} from 'mongoose'
 const EmployeeSchema = new Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, index: {unique: true}},
+    phone: {type: String, required: true, index: {unique: true}},
     password: {type: String, required: true}
 })
 
